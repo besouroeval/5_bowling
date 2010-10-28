bd2d015f776a47764311090abcf67fb4facd2da4
somename <someemail>
2010-10-28 14:34:48 -0200

    besouro automatic message

--- src/Bowling.java
+++ src/Bowling.java
@@ -10,8 +10,13 @@ public class Bowling {
 			int ball1 = game[2*i];
 			int ball2 = game[2*i+1];
 			
-			total += ball1;
-			total += ball2;
+			if (spare) {
+				
+			} else {
+				total += ball1;
+				total += ball2;
+				
+			}
 			
 			if (ball1 + ball2 == 10) {
 				spare = true;
