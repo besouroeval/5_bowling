ed964a2d5f880adb266f09c487389bb459afce73
somename <someemail>
2010-10-28 14:31:21 -0200

    besouro automatic message

--- src/Bowling.java
+++ src/Bowling.java
@@ -3,10 +3,16 @@ public class Bowling {
 
 	public static int score(int[] game) {
 		int total = 0;
+		boolean spare = false;
 		for(int i=0 ; i< game.length/2 ; i++){
 			int frameTotal = 0;
 			frameTotal += game[2*i];
 			frameTotal += game[2*i+1];
+			
+			if (frameTotal == 10) {
+				spare = true;
+			}
+			
 			total += frameTotal;
 		}
 		return total;
