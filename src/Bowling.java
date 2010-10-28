5516155ac979ff1ed299a6548b8ed29996879d0e
somename <someemail>
2010-10-28 14:29:31 -0200

    besouro automatic message

--- src/Bowling.java
+++ src/Bowling.java
@@ -3,8 +3,8 @@ public class Bowling {
 
 	public static int score(int[] game) {
 		int total = 0;
-		for(int i=0 ; i< game.length ; i++){
-			total += turn;
+		for(int i=0 ; i< game.length/2 ; i++){
+			total += game[i];
 		}
 		return total;
 	}
