b22c7e8f3743396b9b5a86e255e4fe62612fc770
somename <someemail>
2010-10-28 14:21:49 -0200

    besouro automatic message

--- test/BowlingTest.java
+++ test/BowlingTest.java
@@ -7,9 +7,7 @@ public class BowlingTest {
 
 	@Test
 	public void shouldComputeZeroGame() {
-		
 		Assert.assertEquals(0, Bowling.score(new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}));
-		
 	}
 	
 }
